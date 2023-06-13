4754c238a053aa0cd44f2b926f7d0e9d313309fc
SeddiMK <web.egorovm@gmail.com>
2023-06-13 14:55:51 +0700

    React-1

---
+++ script-0.js
@@ -0,0 +1,7 @@
+let userName = 'Aleg';
+// let userName = 'Oleg'; //entifier 'userName' has already been declared
+userName = 'Oleg';
+
+const userLastName = 'Tyfany';
+// userLastName = 'Robodog'; //signment to constant variable.
+// const userLastName = 'Robodog'; //entifier 'userLastName' has already been declared
