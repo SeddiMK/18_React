271dafa49495e67636980df80ab3a0497edbc8fc
SeddiMK <web.egorovm@gmail.com>
2023-06-14 16:28:57 +0700

    React_1

--- app_1/src/App.js
+++ app_1/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const colorOrange = 'orange';
+const myObgStyle = { color: colorOrange, fontFamily: 'Georgia' };
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header big-text">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code style={myObgStyle}>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div></div>
+    </>
   );
 }
 
