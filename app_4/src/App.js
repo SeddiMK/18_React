10655ae5e2a980bf7cc04d9ed475022090820030
SeddiMK <web.egorovm@gmail.com>
2023-06-19 14:49:38 +0700

    React-4_input_events

--- app_4/src/App.js
+++ app_4/src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  // let a = 55;
+  let textInput = React.createRef();
+  let textOut = React.createRef();
+
+  function f1(arg) {
+    console.log('f1 - works ' + arg);
+  }
+  function f2() {
+    console.log('f2 - mouse move');
+  }
+  function showInput(event) {
+    console.log('input');
+    // console.log(this.value); // this??
+    // console.log(event.target.value);
+    console.log(textInput.current.value);
+    textOut.current.innerHTML = textInput.current.value;
+  }
   return (
     <>
       <h2>События</h2>
+      <section>
+        <h3>Button</h3>
+        <button onClick={() => f1('button')}>Push</button>
+      </section>
+      <section>
+        <h3>Duble click + mouse move</h3>
+        <div className="test" onDoubleClick={() => f1(44 + ' orange')}></div>
+      </section>
+      <section>
+        <h3>Input</h3>
+        <input
+          type="text"
+          onInput={showInput}
+          ref={textInput}
+          defaultValue="hello"
+        />
+        <p ref={textOut}></p>
+      </section>
     </>
   );
 }
