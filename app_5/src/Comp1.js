4792ed9550ecf968a5979d1e7ee2fa3167441d74
SeddiMK <web.egorovm@gmail.com>
2023-06-19 15:39:42 +0700

    React-5_State

---
+++ app_5/src/Comp1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Comp1 extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>State</h1>
+        <div>
+          <button>This button change State</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Comp1;
