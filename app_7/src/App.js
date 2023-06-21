5309bb119751a52442831384336e7a9d14f921b7
SeddiMK <web.egorovm@gmail.com>
2023-06-21 19:09:28 +0700

    React_7-component_life_cycle

--- app_7/src/App.js
+++ app_7/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Test1 from './Test1';
+import Test1 from './Test1Class';
 import Test2 from './Test2Function';
 
 function App() {
   return (
     <>
-      {/* <Test1 arg={86} /> */}
+      <Test1 arg={86} />
       <hr />
       <Test2 arg={93} />
     </>
